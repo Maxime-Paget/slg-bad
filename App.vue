05f865506a04ac67a4f739949e0fc25b71f8ee61
Maxime-Paget <maxime.paget26@gmail.com>
2025-03-23 19:21:18 +0100

    Initialize project with the app topbar

---
+++ App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">
+        <TopBar></TopBar>
+        <section id="home" class="home">
+
+        </section>
+        <section id="planning" class="planning"></section>
+        <section id="contact" class="contact"></section>
+    </div>
+</template>
+
+<script setup lang="ts">
+import TopBar from './public/components/TopBar.vue';
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ public/components/TopBar.vue
@@ -0,0 +1,71 @@
+<template>
+    <header class="topbar">
+        <img src="../assets/icons/logo.svg" alt="logo" class="topbar__logo">
+        <nav class="topbar__redirects">
+            <ul class="topbar__redirects__list">
+                <li class="topbar__redirects__list__item">
+                    <NuxtLink to="/#home">Accueil</NuxtLink>
+                </li>
+                <li class="topbar__redirects__list__item">
+                    <NuxtLink to="/#planning">Planning</NuxtLink>
+                </li>
+                <li class="topbar__redirects__list__item">
+                    <NuxtLink to="/#contact">Contact</NuxtLink>
+                </li>
+                <li class="topbar__redirects__list__item">
+                    <NuxtLink to="/profile">Mon Profil</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style lang="scss">
+@use '../assets/sass/abstract' as *;
+
+.topbar {
+    background-color: var(--clr-background-dark-high);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-direction: column;
+    max-height: 10%;
+    @include gap(sm);
+    @include padding(sm);
+
+    &__logo {
+        aspect-ratio: 1/1;
+        height: 76px;
+    }
+
+    &__redirects__list {
+        text-transform: uppercase;
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        @include gap(sm);
+
+        a {
+            color: var(--clr-text-light-medium);
+            text-decoration: none;
+        }
+    }
+}
+
+@media screen and (min-width: 1080px) {
+    .topbar {
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        max-height: 10%;
+    }
+}
+
+@media screen and (max-width: 430px) {
+    .topbar {
+        &__redirects__list {
+            display: none;
+        }
+    }
+}
+</style>
