3737106973290551badb134daac4d8e9eaf7dd0d
Maxime-Paget <maxime.paget26@gmail.com>
2025-03-31 21:08:49 +0200

    Set the config for gh-pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/public/assets/sass/main.scss'],
-  
+  vite: {
+    base: '/slg-bad'
+  },
   app: {
     head: {
       title: 'SLG Badminton - Limoges',
